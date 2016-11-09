5e799f05ace1401c55174283e4b2424ccfaf3d32
tinyz <zou90512@126.com>
2016-11-10 01:37:26 +0800

    :two::zero::one::six::arrow_right::one::one::one::zero:

---
+++ Views/JxcProductInfo.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * 管理产品基本信息.
+ *
+ */
+include_once "../Templates/include.php";
+
+use Jxc\Impl\Core\JxcConfig;
+use Jxc\Impl\Dao\CustomerDao;
+
+$dao = new CustomerDao(JxcConfig::$DB_Config);
+$resultSet = $dao->selectCustomNameList();
+$custom_list = array();
+foreach ($resultSet as $k => $v) {
+    $custom_list[] = array('id' => $k, 'text' => $v['ct_name']);
+}
+$pub_custom_list = json_encode($custom_list);
+//
+$remoteUrl = "../Jxc/index.php?api=mg_product";
+?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+<body id="body">
+</body>
+<script>
+    $(document).ready(function () {
+        var content = $('#div_main_cnt').w2grid({
+            name: 'div_main_cnt',
+            header: '产品信息管理',
+            multiSelect: true,
+            url: "<?=$remoteUrl ?>",
+            columns: [
+                {field: 'pdt_id', caption: '编号', size: '10%', editable: {type: 'text'}},
+                {field: 'pdt_name', caption: '名称', size: '10%', editable: {type: 'text'}},
+                {field: 'pdt_color', caption: '颜色', size: '5%', editable: {type: 'text'}},
+                {field: 'pdt_price', caption: '进货价', size: '5%', editable: {type: 'text'}}
+            ],
+            show: {
+                toolbar: true,
+                toolbarAdd: true,
+                toolbarSave: true,
+                toolbarDelete: true,
+                lineNumbers: true,
+                footer: true
+            },
+            toolbar: {
+                items: [
+                    {type: 'break'},
+                    {type: 'button', id: 'mybutton', caption: 'My other button', img: 'icon-folder'},
+                    {type: 'button', id: 'newLogSales', caption: '新增销售记录',},
+                    {
+                        type: 'menu', id: 'menuPdt', caption: '货号2',
+                        items: ["xxxx", "yyyy"],
+                        options: {
+                            url: "../Jxc/index.php?api=get_pdt_id_list",
+                            postData: {
+                                'moduleId': 'menuPdt',
+                                'aryPdtId': '1'
+                            }
+                        }
+                    }
+                ],
+                onClick: function (target, data) {
+                    console.log(target);
+                }
+            },
+            onSubmit: function (event) {
+                var pdt_id = w2GridCheckUniqueID(this, 'pdt_id');
+                if (pdt_id) {
+                    event.preventDefault();
+                    w2alert("[Error]货号[" + pdt_id + "]重复, 请重新输入.", "Error");
+                }
+            },
+            onLoad: function (event) {
+                w2uiInitEmptyGrid(this, event);
+            },
+            onAdd: w2GridOnAdd,
+            onEditField: w2GridOnEditField,
+            onSave: w2GridOnSaveAndUpdate,
+            onKeydown: w2GridOnKeyDown
+        });
+        w2ui['layout'].content('main', content);
+
+//        $.ajax
+
+        //  根据货号筛选
+        $.getJSON("../Jxc/index.php?api=get_pdt_id_list", null, function (data) {
+            if (data['status'] == 'success') {
+                var item = {
+                    type: 'menu', id: 'selectPdt', caption: '选择货号',
+                    items: data['items']
+                };
+                w2ui['div_frame'].toolbar.add(item);
+                w2ui['div_frame'].toolbar.refresh();
+            }
+        });
+    });
+</script>
+</html>
