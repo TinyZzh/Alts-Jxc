1b94d2eb8382a9fcc2d5e750625c9a9a0e90ee31
TinyZ <tinyzzh815@gmail.com>
2016-12-07 18:20:08 +0800

    20161205-1  增加HTTP请求

--- login.php
+++ login.php
@@ -2,15 +2,40 @@
 <html lang="zh-cn">
 <head>
     <meta charset="UTF-8">
-    <meta name="title" content="奥蕾泰森 - 进销存管理系统">
-    <title>奥蕾泰森 - 进销存管理系统</title>
+    <meta name="title" content="奥蕾泰森 - 管理登录">
+    <title>奥蕾泰森 - 管理登录</title>
     <link href="css/w2ui-1.4.3.min.css" type="text/css" rel="stylesheet">
     <script src="js/jquery.min.js" type="text/javascript"></script>
     <script src="js/w2ui-1.4.3-zh-cn.js" type="text/javascript"></script>
 </head>
 <body id="body">
 <div id="layout">
-    <div id="div_main"></div>
+    <div id="div_main" style="width: 300px"></div>
+
+
+    <div class="login-screen">
+        <div class="login-icon">
+            <img src="img/login/icon.png" alt="Welcome to Mail App">
+            <h4>Welcome to <small>Mail App</small></h4>
+        </div>
+
+        <div class="login-form">
+            <div class="form-group">
+                <input type="text" class="form-control login-field" value="" placeholder="Enter your name" id="login-name">
+                <label class="login-field-icon fui-user" for="login-name"></label>
+            </div>
+
+            <div class="form-group">
+                <input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass">
+                <label class="login-field-icon fui-lock" for="login-pass"></label>
+            </div>
+
+            <a class="btn btn-primary btn-lg btn-block" href="#">Log in</a>
+            <a class="login-link" href="#">Lost your password?</a>
+        </div>
+    </div>
+
+
 </div>
 <script type="text/javascript">
     //  locale
@@ -33,26 +58,27 @@
             }
         }
     });
+    $('#div_main').w2render('jxc_login_form');
     //    w2ui.layout.content('main', login);
 
-    w2popup.open({
-        title: '登 录',
-        body: '<div id="pop_layout" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px;"></div>',
-        width: 400,
-        height: 200,
-        showClose: false,
-        modal: true,
-        onOpen: function (event) {
-            console.log(event);
-            event.onComplete = function (event) {
-                console.log(event);
-                // if (obj.options.max) {
-                //     onPopupMax();
-                // }
-                $('#pop_layout').w2render('jxc_login_form');
-            };
-        }
-    });
+//    w2popup.open({
+//        title: '登 录',
+//        body: '<div id="pop_layout" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px;"></div>',
+//        width: 400,
+//        height: 200,
+//        showClose: false,
+//        modal: true,
+//        onOpen: function (event) {
+//            console.log(event);
+//            event.onComplete = function (event) {
+//                console.log(event);
+//                // if (obj.options.max) {
+//                //     onPopupMax();
+//                // }
+//                $('#pop_layout').w2render('jxc_login_form');
+//            };
+//        }
+//    });
 
 
 </script>
